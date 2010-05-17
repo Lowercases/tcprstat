bdea4b7ecb4f810365fa61e454e88c77be8a25f5
Ignacio Nin <nacho@ivana.(none)>
2010-05-17 18:33:57 -0300

    Output

---
+++ src/output.c
@@ -0,0 +1,145 @@
+ /**
+ *   rtime -- Extract stats about TCP response times
+ *   Copyright (C) 2010  Ignacio Nin
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+**/
+ 
+#include "output.h"
+#include "stats.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+static int output(char format[]);
+
+void *
+output_thread(void *arg) {
+    struct output_options *options;
+    struct timespec ts;
+    
+    options = arg;
+    
+    ts = (struct timespec) { options->interval, 0 };
+    
+    if (!check_format(options->format))
+        abort();
+    
+    do {
+        nanosleep(&ts, NULL);
+        
+        output(options->format);
+        
+    }
+    while (1);
+    
+    return NULL;
+    
+}
+
+static int
+output(char format[]) {
+    char *c;
+    
+    struct stats_results *results;
+    
+    results = get_flush_stats();
+    
+    for (c = format; c[0]; c ++)
+        if (c[0] == '%') {
+            int r = 100;
+            c ++;
+            
+            if (c[0] >= '0' && c[0] <= '9') {
+                r = 0;
+                while (c[0] >= '0' && c[0] <= '9') {
+                    r *= 10;
+                    r += c[0] - '0';
+                    
+                    c ++;
+                    
+                }
+                
+            }
+            
+            if (c[0] == 'n')
+                printf("%u", stats_count(results, r));
+            else if (c[0] == 'a')
+                printf("%lu", stats_avg(results, r));
+            else if (c[0] == '%')
+                fputc(c[0], stdout);
+            
+        }
+        else if (c[0] == '\\')
+            if (c[1] == 'n') {
+                c ++;
+                fputc('\n', stdout);
+            }
+            else if (c[1] == 't') {
+                c ++;
+                fputc('\t', stdout);
+            }
+            else if (c[1] == '\\') {
+                c ++;
+                fputc('\\', stdout);
+            }
+            else
+                fputc('\\', stdout);
+            
+        else
+            fputc(c[0], stdout);
+        
+    free_results(results);
+    
+    return 0;
+
+}
+
+int
+check_format(char format[]) {
+    char *c;
+    
+    for (c = format; *c; c ++)
+        if (c[0] == '%') {
+            int r = -1;
+            c ++;
+            
+            if (c[0] >= '0' && c[0] <= '9') {
+                r = 0;
+                while (c[0] >= '0' && c[0] <= '9') {
+                    r *= 10;
+                    r += c[0] - '0';
+                    
+                    c ++;
+                    
+                }
+                
+                if (r <= 0 || r > 100)
+                    return 0;
+                
+            }
+            
+            if (c[0] != 'n' && c[0] != 'a' && (c[0] != '%' || r != -1))
+                return 0;
+            
+        }
+        
+    return 1;
+    
+}
+
---
+++ src/output.h
@@ -0,0 +1,36 @@
+/**
+ *   rtime -- Extract stats about TCP response times
+ *   Copyright (C) 2010  Ignacio Nin
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+**/
+
+#if !defined(OUTPUT_H)
+#define OUTPUT_H
+
+struct output_options {
+    char *format;
+    int interval;       // Seconds
+    
+};
+
+// Parameter of output_thread is of type output_thread_options
+void *output_thread(void *arg);
+int check_format(char format[]);
+
+#endif
+
